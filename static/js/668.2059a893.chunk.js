6aaefb88f821f96155a646d401c6aadd2168d547
OlesiaKubska <120682505+OlesiaKubska@users.noreply.github.com>
2023-10-17 20:47:46 +0000

    Deploying to gh-pages from @ OlesiaKubska/goit-react-hw-08-phonebook@3f983851a536e4dbfcdde86b2358be5fabfa4472 🚀

---
+++ static/js/668.2059a893.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[668],{668:function(e,n,t){t.r(n);t(791);var o=t(184);n.default=function(){return(0,o.jsx)("div",{children:(0,o.jsx)("h1",{children:"Contacts"})})}}}]);
+//# sourceMappingURL=668.2059a893.chunk.js.map
