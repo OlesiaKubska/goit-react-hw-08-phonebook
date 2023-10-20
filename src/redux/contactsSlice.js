764f54764beb88f85c53aed1acda3ba677c0e698
OlesiaKubska <kublesia0908@gmail.com>
2023-10-20 15:28:46 +0200

    update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -4,10 +4,7 @@ import { fetchContacts, addContact, deleteContact } from "./operations";
 const contactsSlice = createSlice({
     name: "contacts",
     initialState: {
-        items: [{ "id": 1, "name": "Rosie Simpson", "number": "459-12-56" },
-            { "id": 2, "name": "Hermione Kline", "number": "443-89-12" },
-            { "id": 3, "name": "Eden Clements", "number": "645-17-79" },
-            { "id": 4, "name": "Annie Copeland", "number": "227-91-26" }],
+        items: [],
         isLoading: false,
         error: null,
     },
