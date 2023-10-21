9ffe6cc1a965a1fe119deb38dcb0c7a6fa5e6dea
OlesiaKubska <kublesia0908@gmail.com>
2023-10-21 08:58:49 +0200

    Update Contacts.jsx

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { useDispatch } from 'react-redux';
 import { Section } from 'components/Section/Section';
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import { Filter } from 'components/Filter/Filter';
 import { ContactList } from 'components/ContactList/ContactList';
 import { Header } from 'components/Header/Header';
+import { fetchContacts } from 'redux/operations';
 
 const Contacts = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchContacts());
+    }, [dispatch]);
+
     return (
         <Section title="PhoneBook">
             <ContactForm />
